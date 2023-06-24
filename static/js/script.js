362d24f0d09e424c67e0f0a3fcbbdadab81d652d
removedporn <86824510+removedporn@users.noreply.github.com>
2023-06-24 10:50:07 +0800

    Delete static directory

--- static/js/script.js
+++
@@ -1,8 +0,0 @@
-submitted = document.getElementById("submitted");
-
-document.getElementById("upload_submit").addEventListener("click", function () {
-  document.getElementById("upload_submit").classList.add("sr-only");
-  submitted.classList.remove("submitted-hidden");
-  submitted.classList.add("submitted-visible");
-  return console.log("upload click me");
-});
